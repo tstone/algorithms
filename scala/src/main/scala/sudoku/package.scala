384020c95d64820ea92cfb2d3a4477036269d6a1
Titus Stone <wastingtape@gmail.com>
2014-09-03 23:44:04 -0600

    Added ability to solve sudoku puzzles

--- scala/src/main/scala/sudoku/package.scala
+++ scala/src/main/scala/sudoku/package.scala
@@ -8,8 +8,14 @@ package object sudoku {
 
   implicit class GameBoardOps(board: GameBoard) {
 
-    def row(c: Int) = board(c - 1).flatten.toSet
-    def col(r: Int) = board.map { col => col(r - 1) }.flatten.toSet
+    def row(r: Int) = board(r - 1).flatten.toSet
+    def col(c: Int) = board.map { col => col(c - 1) }.flatten.toSet
+
+    def point(x: Int, y: Int): Option[Int] =
+      board(y - 1)(x - 1)
+
+    def point(x: Int, y: Int, value: Option[Int]): GameBoard =
+      board.updated(y - 1, board(y - 1).updated(x - 1, value))
 
     def subBoardAt(x: Int, y: Int): Set[Int] = {
       def translate(pos: Int) = (pos - 1) * 3
@@ -39,6 +45,62 @@ package object sudoku {
           )
         )
     }
+
+    def toPrettyString = board.zipWithIndex.map { case (col, x) =>
+      col.zipWithIndex.map { case (point, y) =>
+        val padding = if (y % 3 == 0) "  " else ""
+        val break = if (x % 3 == 0 && y == 0) "\n" else ""
+        break + padding + point.getOrElse("/")
+      }.mkString(" ")
+    }.mkString("\n") + "\n"
+
+    def isSolved: Boolean =
+      board.forall(_.forall(_.isDefined))
+
+    def firstUnsolvedPoint: (Int, Int) = {
+      val (y, x) = board.zipWithIndex.map { case (row, r) =>
+        row.zipWithIndex.collectFirst { case (None, c) => (r, c)}
+      }.flatten.head
+      (x + 1, y + 1)
+    }
+
+    def solve: Option[GameBoard] = {
+      // pick off the low-hanging fruit by plugging in points
+      // for which there is only one possiblity
+      val newBoard = singlePossibilties
+      if (newBoard.isSolved) Some(newBoard)
+      else {
+        // Start with the first unsolved point and exhaustivley
+        // attempt all possibitlies for remaining unsolved points
+        val (x, y) = firstUnsolvedPoint
+        multiPossibilitiesFor(x, y, possibilitiesAt(x, y).toList)
+      }
+    }
+
+    protected def singlePossibilties: GameBoard = {
+      val (newBoard, totalChanged) = board.zipWithIndex.foldLeft((board, 0)) { case ((b, c), (col, y)) =>
+        col.zipWithIndex.filter(_._1.isEmpty).foldLeft((b, c)) { case ((currentBoard, changed), (_, x)) =>
+          val ps = currentBoard.possibilitiesAt(x + 1, y + 1)
+          if (ps.size == 1) (currentBoard.point(x + 1, y + 1, Some(ps.toSeq(0))), changed + 1)
+          else (currentBoard, changed)
+        }
+      }
+
+      if (newBoard.isSolved) newBoard
+      else if (totalChanged > 0) newBoard.singlePossibilties
+      else newBoard
+    }
+
+    protected def multiPossibilitiesFor(x: Int, y: Int, possibilties: Seq[Int]): Option[GameBoard] = possibilties match {
+      case Nil          => None
+      case head :: tail =>
+        val guessBoard = board.point(x, y, Some(head))
+        guessBoard.solve match {
+          case None     => multiPossibilitiesFor(x, y, tail)
+          case solution => solution
+        }
+    }
+
   }
 
   private def union[A](sets: Set[A]*) =
--- scala/src/test/scala/sudoku/sudokuSpec.scala
+++ scala/src/test/scala/sudoku/sudokuSpec.scala
@@ -9,17 +9,17 @@ class sudokuSpec extends BaseSpec {
     /*
         Game Board:
 
-        1  2  3   N  N  N   N  N  N
-        4  5  6   1  2  3   N  N  N
-        7  8  9   N  N  N   N  N  N
+        1  2  3   /  /  /   /  /  /
+        4  5  6   1  2  3   /  /  /
+        7  8  9   /  /  /   /  /  /
 
-        N  N  N   N  N  N   N  N  N
-        N  N  N   N  N  N   N  N  N
-        N  N  N   N  N  N   N  N  N
+        /  /  /   /  /  /   /  /  /
+        /  /  /   /  /  /   /  /  /
+        /  /  /   /  /  /   /  /  /
 
-        N  N  N   N  N  N   N  N  N
-        N  N  N   N  N  N   N  N  N
-        N  N  N   N  N  N   N  N  N
+        /  /  /   /  /  /   /  /  /
+        /  /  /   /  /  /   /  /  /
+        /  /  /   /  /  /   /  /  /
 
      */
 
@@ -51,12 +51,26 @@ class sudokuSpec extends BaseSpec {
       Seq(Some(3), None, None, Some(9), None, Some(2), None, None, Some(5))
     )
 
+    val solved = Seq(
+      Seq(Some(2), Some(9), Some(6), Some(3), Some(1), Some(8), Some(5), Some(7), Some(4)),
+      Seq(Some(5), Some(8), Some(4), Some(9), Some(7), Some(2), Some(6), Some(1), Some(3)),
+      Seq(Some(7), Some(1), Some(3), Some(6), Some(4), Some(5), Some(2), Some(8), Some(9)),
+      Seq(Some(6), Some(2), Some(5), Some(8), Some(9), Some(7), Some(3), Some(4), Some(1)),
+      Seq(Some(9), Some(3), Some(1), Some(4), Some(2), Some(6), Some(8), Some(5), Some(7)),
+      Seq(Some(4), Some(7), Some(8), Some(5), Some(3), Some(1), Some(9), Some(2), Some(6)),
+      Seq(Some(1), Some(6), Some(7), Some(2), Some(5), Some(3), Some(4), Some(9), Some(8)),
+      Seq(Some(8), Some(5), Some(9), Some(7), Some(6), Some(4), Some(1), Some(3), Some(2)),
+      Seq(Some(3), Some(4), Some(2), Some(1), Some(8), Some(9), Some(7), Some(6), Some(5))
+    )
+
     "return the row set" in {
       board.row(1) mustEqual Set(1, 2, 3)
       board.row(2) mustEqual Set(1, 2, 3, 4, 5, 6)
       board.row(9) mustEqual Set.empty
 
       board2.row(9) mustEqual Set(2, 3, 5, 9)
+
+      solved.row(6) mustEqual Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
 
     "return the column set" in {
@@ -65,6 +79,8 @@ class sudokuSpec extends BaseSpec {
       board.col(9) mustEqual Set.empty
 
       board2.col(4) mustEqual Set(4, 9)
+
+      solved.col(2) mustEqual Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
 
     "return the sub board location" in {
@@ -80,6 +96,8 @@ class sudokuSpec extends BaseSpec {
 
       board2.subBoardAt(9, 9) mustEqual Set(5, 9)
       board2.subBoardAt(1, 1) mustEqual Set(1, 8)
+
+      solved.subBoardAt(2, 7) mustEqual Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
     }
 
     "return the possibilities based on the input data" in {
@@ -89,7 +107,44 @@ class sudokuSpec extends BaseSpec {
 
       board2.possibilitiesAt(5, 1) mustEqual Set(1, 5, 9)
       board2.possibilitiesAt(4, 2) mustEqual Set(1, 2, 3, 5, 7, 8)
+
+      solved.possibilitiesAt(3, 7) mustEqual Set(7)
     }
 
+    "find the first unsolved point" in {
+      board.firstUnsolvedPoint mustEqual (4, 1)
+      board2.firstUnsolvedPoint mustEqual (2, 1)
+    }
+
+    "get values" in {
+      solved.point(9, 9) must beSome(5)
+      solved.point(1, 1) must beSome(2)
+      solved.point(9, 1) must beSome(4)
+      solved.point(1, 9) must beSome(3)
+      solved.point(4, 5) must beSome(4)
+      solved.point(5, 4) must beSome(9)
+      solved.point(6, 3) must beSome(5)
+      solved.point(3, 6) must beSome(8)
+    }
+
+    "set values" in {
+      val nb1 = solved.point(1, 9, Some(5))
+      nb1(8)(0) must beSome(5)
+      nb1.point(1, 9) must beSome(5)
+
+      val nb2 = solved.point(9, 1, Some(8))
+      nb2(0)(8) must beSome(8)
+      nb2.point(9, 1) must beSome(8)
+
+      val nb3 = solved.point(4, 5, Some(1))
+      nb3(4)(3) must beSome(1)
+      nb3.point(4, 5) must beSome(1)
+    }
+
+    "solve the board" in {
+      val PerformanceResult(r, t) = time { board2.solve }
+      println(s"Sudoku solver took $t s\n" + r.get.toPrettyString)
+      r must beSome
+    }
   }
 }
