884096622c4e86b97aa0a5c348e21bea49098731
Titus Stone <wastingtape@gmail.com>
2014-10-11 17:29:50 -0600

    Finished sudoku clojure code, it actually works now ;)

--- clojure/src/sudoku.clj
+++ clojure/src/sudoku.clj
@@ -1,20 +1,7 @@
-(ns sudoku
-  (:require [clojure.math.numeric-tower :as math]))
+(ns sudoku)
 
-
-
-(def board [
-  [ 1   2   3    nil nil nil   nil nil nil]
-  [ 4   5   6     1   2   3    nil nil nil]
-  [ 7   8   9    nil nil nil   nil nil nil]
-
-  [nil nil nil   nil nil nil   nil nil nil]
-  [nil nil nil    4   5   6    nil nil nil]
-  [nil nil nil   nil nil nil   nil nil nil]
-
-  [nil nil nil   nil nil nil   nil nil nil]
-  [nil nil nil   nil nil nil   nil nil nil]
-  [nil nil nil   nil nil nil   nil nil nil] ])
+(require '[clojure.math.numeric-tower :as math])
+(require '[clojure.set :as set])
 
 
 ;; Helper Functions
@@ -22,8 +9,8 @@
 (defn identity-set
   "Transform the given collection into a set with no nils"
   [xs]
-  (filter identity
-    (into #{} xs)))
+  (into #{}
+    (filter identity xs)))
 
 
 ;; Board Read Functions
@@ -48,7 +35,7 @@
 (defn which-sub-board
   "Determine the sub-board based on a whole-board position"
   [x y]
-  [(math/floor (/ x 3)), (math/floor (/ y 3))])
+  [(int (math/floor (/ x 3))), (int (math/floor (/ y 3)))])
 
 (defn sub-board
   "Get the sub-board at the given x/y"
@@ -58,13 +45,30 @@
         right  (+ left 3)
         top    (* sub-board-y 3)
         bottom (+ top 3)
-        rows   (subvec top bottom board)
-        cols   (map (partial subvec left right) rows)]
+        rows   (subvec board top bottom)
+        cols   (map #(subvec %1 left right) rows)]
     (identity-set
       (flatten cols))))
 
 (defn possibilities
   "Determine the possibilities for the given x/y"
   [board x y]
-  (difference #{1 2 3 4 5 6 7 8 9}
-    (union (row y) (column x) (sub-board x y))))
+  (set/difference #{1 2 3 4 5 6 7 8 9}
+    (row board y) (column board x) (sub-board board x y)))
+
+
+;; Example Board
+;; (also used for repl)
+
+(def board [
+  [ 1   2   3    nil nil nil   nil nil nil]
+  [ 4   5   6     1   2   3    nil nil nil]
+  [ 7   8   9    nil nil nil   nil nil nil]
+
+  [nil nil nil   nil nil nil   nil nil nil]
+  [nil nil nil    4   5   6    nil nil nil]
+  [nil nil nil   nil nil nil   nil nil nil]
+
+  [nil nil nil   nil nil nil   nil nil nil]
+  [nil nil nil   nil nil nil   nil nil nil]
+  [nil nil nil   nil nil nil   nil nil nil] ])
